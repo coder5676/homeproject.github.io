6b4f41d0f1a475ee5d36ae784e8dd6ea638560ca
Abhai maurya <148702223+coder5676@users.noreply.github.com>
2024-07-31 21:53:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let temppopup=document.getElementById("temppop");
-    document.addEventListener("click",()=>{
+   document.addEventListener("click",()=>{
          document.documentElement.requestFullscreen();
     });
     /*open temp popup*/
-       
     function opentemp(){
         temppopup.classList.add("open-temppop");
-    }
+    };
     function closetemp(){
         temppopup.classList.remove("open-temppop");
-    }
-    
+    };
     var rem="none";
     var about="none";
+    var speakreminder="You dont have any current reminders";
     function config(){
         var remh=document.getElementById("hr").value;
         var remm=document.getElementById("mi").value;
@@ -29,6 +28,7 @@ let temppopup=document.getElementById("temppop");
     } function save(){
         [rem,about]=config();
         document.getElementById("setreminder").classList.remove("opensetreminder");
+        speakreminder="you have a reminder that says"+about;
     }
     function stop(){
          rem="none";
@@ -57,6 +57,9 @@ let temppopup=document.getElementById("temppop");
         rem=String(later);
         document.getElementById("reminderaudio").pause();
     }
+    var speakday="none";
+    var speaktime="none";
+    var hello="hi how are you";
     function showtime(){
         let time=new Date;
         let day=time.getDay();
@@ -74,6 +77,9 @@ let temppopup=document.getElementById("temppop");
         let currenttime=hours+":"+min;
         var currtime=String(hours)+":"+String(min);
         week=["Sun","Mon","Tue","Wed","Thr","Fri","Sat"];
+        montharr=["january","february","march","April","May","june","july","august","september","october","november","december"]
+        speakday=week[day]+""+date+""+montharr[month];
+        speaktime=hours+"hours"+min+"minutes";
         document.getElementById("time").innerHTML=currenttime;
         document.getElementById("daydate").innerHTML=week[day]+" "+date;
         document.getElementById("hm").innerHTML=hours+":"+min;
@@ -87,7 +93,7 @@ let temppopup=document.getElementById("temppop");
         else if (hours>=12 && hours <16) {
             text='Good afternoon ';
             document.getElementById("temppop").style.backgroundColor="#5A90E2";
-            document.getElementById("bgimage").innerHTML="⛱️";
+            document.getElementById("bgimage").innerHTML="🌾";
             document.getElementById("sun").innerHTML="☀️";
         }else if(hours>=16 && hours<19){
             text='Good evening';
@@ -99,11 +105,12 @@ let temppopup=document.getElementById("temppop");
         else {
             text="Good night";
             document.getElementById("temppop").style.backgroundColor="#1D1632";
-            document.getElementById("bgimage").innerHTML="🏕️";
+            document.getElementById("bgimage").innerHTML="🌑";
             document.getElementById("sun").innerHTML="✨";
             document.getElementById("temp").classList.add("nightsky");
         }
         document.getElementById("greet").innerHTML=text;
+        hello="hello "+text +"how are you";
         if(String(currtime)==String(rem)){
             document.getElementById("remainderbox").classList.add("openremainder");
             document.getElementById("mainarea").innerHTML=about;
@@ -115,15 +122,15 @@ let temppopup=document.getElementById("temppop");
     const apikey="771268cf58226d55a8385e574cac8de9";
     /*const apikey="9f75e97e657f0c97be3d8528384cef21";*/
     const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric";
-    
+    var speaktemp="sorry the temperature is not available";
     async function checkweather(city){
         const response=await fetch(apiurl+`&appid=${apikey}`+`&q=${city}`);
         var data= await response.json();
         console.log(data);
         temp=Math.round(data.main.temp)+"°";
-        const desc=["clear sky","few clouds","scattered clouds","broken clouds","shower rain","rain","thunderstorm","snow","mist","overcast clouds","light rain"];
-        const descicon=["🪂","🌤️","🌨️","🌥️","🌧️","☔","⛈️","☃️","😶‍🌫️","☁️","🌧️"];
-        const anim=["nul","nul","nul","nul","raining","raining","raining","nul","nul","nul","raining"];
+        const desc=["clear sky","few clouds","scattered clouds","broken clouds","shower rain","rain","thunderstorm","snow","mist","overcast clouds","light rain","moderate rain","heavy intensity rain"];
+        const descicon=["🪂","🌤️","🌨️","🌥️","🌧️","☔","⚡","☃️","😶‍🌫️","☁️","🌧️","☔","⛈️"];
+        const anim=["nul","nul","nul","nul","raining","raining","raining","nul","nul","nul","raining","raining","raining"];
 
         document.getElementById("tempno").innerHTML=temp;
         document.getElementById("tempfont").innerHTML=temp;
@@ -134,6 +141,7 @@ let temppopup=document.getElementById("temppop");
         document.getElementById("description").innerHTML=data.weather[0].description;
         document.getElementById("location").innerHTML=city;
         document.getElementById("feelslike").innerHTML="Feels like : "+data.main.feels_like+"°";
+        speaktemp="today's temperature in"+city+"is"+temp+"with"+data.weather[0].description+"and humidity of"+data.main.humidity+"percent";
         const d=data.weather[0].description;
         let position=desc.indexOf(d);
         console.log(position);
@@ -157,51 +165,53 @@ let temppopup=document.getElementById("temppop");
         document.getElementById("newsframe").src="";
         document.getElementById("swipe").innerHTML="&#8592";
         
-    } 
+    } ;
     function openrecipe(){
         document.getElementById("recipesbox").classList.add("openrecipebox");
         document.getElementById("swipe").innerHTML="&#8594";
-    }
+    };
     function opennews(){
         document.getElementById("newsframe").classList.add("opennewsframe");
         document.getElementById("newsframe").src="https://www.ndtv.com/";
        
         document.getElementById("swipe").innerHTML="&#8594";
-    }
+    };
     function openclock(){
         document.getElementById("clockbox").classList.add("openclockbox");
-    }
+        
+    };
     function closeclock(){
         document.getElementById("clockbox").classList.remove("openclockbox");
-    }function ta(){
+    };
+    function ta(){
         document.getElementById("clockbox").style.background="linear-gradient(140deg,#C4E759,#6DE195)";
        
-    }
+    };
     function tb(){
         document.getElementById("clockbox").style.background="linear-gradient(240deg,#C1E3FF,#41C7AF)";
        
 
-    }
+    };
     function tc(){
         document.getElementById("clockbox").style.background="linear-gradient(200deg,#8DEBFF,#6CACFF)";
        
-    }
+    };
     function td(){
         document.getElementById("clockbox").style.background="linear-gradient(240deg,#DEB0DF,#A16BFE)";
         
         
-    }
+    };
     function te(){
         document.getElementById("clockbox").style.background="linear-gradient(140deg,#FDEB82,#F78FED)";
         
         
-    }function openthemewindow(){
+    };function openthemewindow(){
         document.getElementById("themeswindow").classList.toggle("openthemewindow");
-    }
+    };
     function openmusic(){
         const a=document.getElementById("musican");
         a.setAttribute("href","https://www.jiosaavn.com/");
-    }
+    };
    function openremainder(){
         document.getElementById("setreminder").classList.add("opensetreminder");
     };
@@ -209,9 +219,7 @@ let temppopup=document.getElementById("temppop");
         document.getElementById("setreminder").classList.remove("opensetreminder");
 
     };
-    function openassistant(){
-        document.getElementById("assistantbox").classList.toggle("openassistantbox");
-    };
+    
     function openwallpaper(){
         document.getElementById("wallpaperpopup").classList.add("openwallpaperwindow");
     };
@@ -240,6 +248,76 @@ let temppopup=document.getElementById("temppop");
         document.getElementById("a").style.backgroundImage="url('img5.jpeg')";
 
     };
+    const SpeechRecognition=window.SpeechRecognition||window.webkitSpeechRecognition;
+    const recognition= new SpeechRecognition();
+    recognition.interimResults=true;
+    recognition.lang='en-IN';
+    recognition.continuous=true;
+    const searchform=document.getElementById("searchform");
+    const searchforminput=searchform.querySelector("input");
+    recognition.onstart=function(){
+        console.log("vr active");
+        document.getElementById("assistantbox").classList.add("openassistantbox");
+       document.getElementById("assistanton").play();
+       result.value="";
+       searchform.focus();
+       
+    }
+    recognition.onend=function(){
+        document.getElementById("assistantbox").classList.remove("openassistantbox");
+        document.getElementById("assistantoff").play();
+        searchform.focus();
+        
+
+    };
+    function openassistant(){
+        recognition.start();
+    };
+    function closeassistant(){
+        recognition.stop();
+    }
+    const result=document.getElementById("request");
+    recognition.addEventListener("result",resultofspeechrecognition);
+    var transcript="";
+    function resultofspeechrecognition(event){
+        transcript=String(event.results[0][0].transcript).toLowerCase();
+        var result=document.getElementById("request");
+        result.value=transcript;
+        searchforminput.value=transcript;
+        /*system related commands*/
+        var keywords=["temperature","the temperature","time","tie","die","ti","try","skype","date","they","baby","day","reminder","remind","hello","hi"];
+        var work=[speaktemp,speaktemp,speaktime,speaktime,speaktime,speaktime,speaktime,speaktime,speakday,speakday,speakday,speakday,speakreminder,speakreminder,hello,hello];
+        var ind=keywords.indexOf(transcript);
+        if(ind==-1){
+            setTimeout(() => {
+                searchform.submit();
+            }, 750);
+        }
+        else{
+            readout(work[ind]);
+        }
+        
+        readout(work[ind]);
+        
+        /*online related commands*/
+        
+        
+        
+    
+
+    };
 
     
+    function readout(message){
+        let speech= new SpeechSynthesisUtterance();
+        let voices=speechSynthesis.getVoices();
+        speech.voice=voices[3];
+        speech.text=message;
+        window.speechSynthesis.speak(speech);
+
+    };
+    
+    
+    
+   
     
